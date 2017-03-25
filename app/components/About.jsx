0a1fbea2bf103a129608d0eeeb54d7243bc51472
Your Name <you@example.com>
2017-03-24 17:54:13 -0700

    Updated

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,7 +11,8 @@ var React=require('react')
 var About=(props)=>{
     return(
         <div>
-            <h1>About!!</h1>
+            <h1>About Page</h1>
+            <p>Welcome to About Page</p>
         </div>
     )
 }
